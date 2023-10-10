2df9ef677ba5540d286a9ee0670e583acf55d945
Lucas Sobral <sobralroxo@gmail.com>
2023-10-10 20:22:19 -0300

    Correção de imagens

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,26 +1,26 @@
 import {} from 'react'
-//importando a estrutura para maninupar as imagens
 import {Swiper, SwiperSlide} from 'swiper/react'
 import {useState, useEffect} from 'react'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import styles from './estilo.module.css';
+import eclipse from '../assets/eclipse.webp';
+import lancerevo from '../assets/lancerevo.jpg';
+import supra from '../assets/supra.webp';
+
 
 
 function Home (){
 
-  //Hooks
-
     const [slidepreview, setSlidepreview] =useState(1);
 
-  // crinado o ojeto de imagems
      const data =[
-      {id: '1', image: './src/assets/supra.webp'},
-      {id: '2', image: './src/assets/lancerevo.jpg'},
-      {id: '3', image: './src/assets/eclipse.webp'},
+      {id: '1', image: supra},
+      {id: '2', image: lancerevo},
+      {id: '3', image: eclipse},
 
     ];
-// useEffect faz o efeito colateral 
+
     useEffect(() =>{
       function handleRezise(){
         if(window.innerWidth < 720){
@@ -31,30 +31,25 @@ function Home (){
       }
       handleRezise();
 
-      //modifica toda vez que o usuario diminuir a tela
+
       window.addEventListener('resize', handleRezise);
       return()=>{
         window.removeEventListener('resize', handleRezise);
       };
-      //retornando um array vazio
+
     },[]);
 
 
   return(
     <section>
       <p>Seja bem vindo a loja de carros mais sofisticada do Brasil, com uma variedade imensa no estoque e preços baixos.</p>
-      <Swiper 
-       slidepreview={slidepreview}
-       pagination={{clickable:true}}
-       navigation
-      >
+      <Swiper slidepreview={slidepreview} pagination={{clickable:true}} navigation>
         {data.map((item)=>(
           <SwiperSlide key={item.id}>
             <img src={item.image} alt="imagem" className={styles.slideItem}/>
           </SwiperSlide>
         ))}
       </Swiper>
-      
     </section>
   )
 }
